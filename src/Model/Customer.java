f36e75a4f39e22a96513dfafb4698ca3addc9d0f
DadeMurphyZC <cfonseca@protonmail.com>
2018-06-04 15:18:48 -0700

    added Customer constructor, getters and setters

--- src/Model/Customer.java
+++ src/Model/Customer.java
@@ -5,10 +5,97 @@
  */
 package Model;
 
+import java.sql.Date;
+import java.sql.Timestamp;
 /**
  *
  * @author cfonseca
  */
 public class Customer {
+    private int id;
+    private String customerName;
+    private int addressId;
+    private int active;
+    private Date createDate;
+    private String createdBy;
+    private Timestamp lastUpdate;
+    private String lastUpdateBy;
+
+    public Customer(int id, String customerName, int addressId, int active) {
+        this.id = id;
+        this.customerName = customerName;
+        this.addressId = addressId;
+        this.active = active;
+        this.createDate = new Date(System.currentTimeMillis());
+        this.createdBy = "test";
+        this.lastUpdate = new Timestamp(System.currentTimeMillis());
+        this.lastUpdateBy = "test";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getLastUpdateBy() {
+        return lastUpdateBy;
+    }
+
+    public void setLastUpdateBy(String lastUpdateBy) {
+        this.lastUpdateBy = lastUpdateBy;
+    }
+    
+    
     
 }
