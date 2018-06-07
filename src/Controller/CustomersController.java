cacecc562ffe449fd1c553cbc21f1e5f414aa92f
DadeMurphyZC <cfonseca@protonmail.com>
2018-06-06 17:02:10 -0700

    added CellValueFactory to tableview columns

--- src/Controller/CustomersController.java
+++ src/Controller/CustomersController.java
@@ -5,14 +5,24 @@
  */
 package Controller;
 
+import Model.Customer;
+import Utils.DBConnection;
 import Utils.SceneObject;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -20,20 +30,46 @@ import javafx.scene.control.Button;
  * @author cfonseca
  */
 public class CustomersController implements Initializable {
-    
-    @FXML Button createUserBtn;
-    
-    @FXML public void addCustomer(Event event) throws IOException{
+
+    @FXML
+    Button createUserBtn;
+    @FXML
+    TableView customerTable;
+    @FXML
+    TableColumn nameCol;
+    @FXML
+    TableColumn addressCol;
+    @FXML
+    TableColumn phoneCol;
+
+    @FXML
+    public static ObservableList customers = FXCollections.observableArrayList();
+
+    @FXML
+    public void addCustomer(Event event) throws IOException {
         SceneObject sc = new SceneObject("AddCustomer.fxml");
-        sc.loadPopup();
+        sc.loadPopup(createUserBtn);
     }
-    
+
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("customerName"));
+        addressCol.setCellValueFactory(new PropertyValueFactory<>("address"));
+        phoneCol.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        try {
+            DBConnection.getCustomers();
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(CustomersController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        DBConnection.customerListDB.forEach((c) -> {
+            customers.add(c);
+        });
+        customerTable.setItems(customers);
+    }
 }
