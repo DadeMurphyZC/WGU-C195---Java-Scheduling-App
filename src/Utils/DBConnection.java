e36677ad6d2e570c53d3814aed906eb12ab732fd
DadeMurphyZC <cfonseca@protonmail.com>
2018-06-13 09:55:07 -0700

    refactored addCustomerToDB() removed lastUpdate parameter as it has a mysql default value in the mysql db

--- src/Utils/DBConnection.java
+++ src/Utils/DBConnection.java
@@ -15,10 +15,6 @@ import java.sql.Statement;
 import javafx.stage.Stage;
 import Model.Customer;
 import java.util.ArrayList;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-
 
 /**
  *
@@ -48,32 +44,32 @@ public class DBConnection {
     }
 
     public static Boolean loginAuth(String u, String p) throws ClassNotFoundException, SQLException, IOException {
-            conn = dbConnect();
-            pstmt = conn.prepareStatement("SELECT * FROM user WHERE userName = ?");
-            pstmt.setString(1, u);
-            rs = pstmt.executeQuery();
-            if (rs.next()) {
-                return p.equals(rs.getString("password"));
-            }
-            return false;
+        conn = dbConnect();
+        pstmt = conn.prepareStatement("SELECT * FROM user WHERE userName = ?");
+        pstmt.setString(1, u);
+        rs = pstmt.executeQuery();
+        if (rs.next()) {
+            return p.equals(rs.getString("password"));
         }
+        return false;
+    }
     public static ArrayList<Customer> customerListDB = new ArrayList();
     public static final Object lock = new Object();
-    
-    public static void getCustomers() throws ClassNotFoundException, SQLException{
+
+    public static void getCustomers() throws ClassNotFoundException, SQLException {
         conn = dbConnect();
         pstmt = conn.prepareStatement("SELECT * from customer");
         rs = pstmt.executeQuery();
-        while (rs.next()){
-            synchronized(lock){
-            Customer temp = new Customer();
-            temp.setCustomerName(rs.getString("customerName"));
-            customerListDB.add(temp);
+        while (rs.next()) {
+            synchronized (lock) {
+                Customer temp = new Customer();
+                temp.setCustomerName(rs.getString("customerName"));
+                customerListDB.add(temp);
             }
         }
     }
-    
-    public static void addCustomerToDB(Customer c) throws ClassNotFoundException, SQLException{
+
+    public static void addCustomerToDB(Customer c) throws ClassNotFoundException, SQLException {
         conn = dbConnect();
         String check = "SELECT * FROM customer WHERE customerName = ?";
         pstmt = conn.prepareStatement(check);
@@ -82,18 +78,18 @@ public class DBConnection {
         if (rs.next()) {
             AlertDiag.loginError();
         } else {
-        pstmt = conn.prepareStatement("INSERT INTO customer "
-                + "(customerId, customerName, addressId, active, createDate, createdBy, lastUpdate, lastUpdateBy)"
-                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+            pstmt = conn.prepareStatement("INSERT INTO customer "
+                    + "(customerId, customerName, addressId, active, createDate, createdBy, lastUpdateBy)"
+                    + "VALUES (?, ?, ?, ?, ?, ?, ?)");
             pstmt.setInt(1, c.getId());
             pstmt.setString(2, c.getCustomerName());
             pstmt.setInt(3, c.getAddressId());
             pstmt.setInt(4, c.getActive());
-            pstmt.setDate(5, c.getCreateDate());
+            pstmt.setDate(5, java.sql.Date.valueOf(c.getCreateDate()));
             pstmt.setString(6, c.getCreatedBy());
-            pstmt.setTimestamp(7, c.getLastUpdate());
-            pstmt.setString(8, c.getLastUpdateBy());
-        pstmt.executeUpdate();
+            //pstmt.setTimestamp(7, c.getLastUpdate());
+            pstmt.setString(7, c.getLastUpdateBy());
+            pstmt.executeUpdate();
         }
     }
 }
