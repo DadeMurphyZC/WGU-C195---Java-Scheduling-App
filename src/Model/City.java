587a34aa5cab4f9f8ef7696f4dc9868ee7bcead2
Cristoval Fonseca <cfonseca@protonmail.com>
2018-11-09 12:38:01 -0800

    Added before insert/update trigger for default values Removed createDate, createdBy, lastUpdateBy from constructor

--- src/Model/City.java
+++ src/Model/City.java
@@ -11,4 +11,37 @@ package Model;
  */
 public class City {
     
+    private int cityId;
+    private String city;
+    private int countryId;
+
+    public City(String city, int countryId, String createdBy, String lastUpdateBy) {
+        this.city = city;
+        this.countryId = countryId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+    
 }
