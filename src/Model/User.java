d66ca82f60f516478e5216e9f9d8be2bd98d0618
Cristoval Fonseca <cfonseca@protonmail.com>
2018-11-09 12:38:20 -0800

    Added before insert/update trigger for default values Removed createDate, createdBy, lastUpdateBy from constructor

--- src/Model/User.java
+++ src/Model/User.java
@@ -11,4 +11,40 @@ package Model;
  */
 public class User {
     
+    private String userName;
+    private String password;
+    private int active;
+
+    public User(String userName, String password, int active) {
+        this.userName = userName;
+        this.password = password;
+        this.active = active;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+    
+    
+    
 }
